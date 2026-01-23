0ac521f191908fd50b11649a93c5f4cef3a80046
Sandeep verma <sandeepverma@Sandeeps-MacBook-Air.local>
2026-01-23 16:52:16 +0530

    add new file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com', 'localhost', '3.238.237.207'],
+    domains: ['res.cloudinary.com', 'localhost', '3.238.237.207', 'www.indianstudiodmc.com', 'indianstudiodmc.com'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -31,7 +31,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://3.238.237.207:8000/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://www.indianstudiodmc.com/api',
     NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || '',
     NEXT_PUBLIC_ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
     NEXT_PUBLIC_ALGOLIA_SEARCH_KEY: process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY || '',
