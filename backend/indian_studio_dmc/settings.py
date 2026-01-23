5daf7072146a8a29c1f30b84ea7c110ac061d350
Sandeep verma <sandeepverma@Sandeeps-MacBook-Air.local>
2026-01-23 18:32:28 +0530

    add new file

--- backend/indian_studio_dmc/settings.py
+++ backend/indian_studio_dmc/settings.py
@@ -100,7 +100,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = env("CORS_ALLOWED_ORIGINS")
+CSRF_TRUSTED_ORIGINS = ["http://*.indianstudiodmc.com", "https://*.indianstudiodmc.com"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
